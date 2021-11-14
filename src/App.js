835e3f1c0e90e4d0aee6e275a68798746cdcf720
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-11-14 16:13:48 -0500

    rougelike code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Canvas from "./components/Canvas";
+
+function App() {
+  return (
+    <div>
+      <Canvas width={40} height={40} tileSize={16}/>
+    </div>
+  );
+}
+
+export default App;
